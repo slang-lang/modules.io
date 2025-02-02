863009f556b622548ffe704c607a3f7a46f1503a
Michael Adelmann <noreply@github.com>
2025-02-02 12:11:30 +0100

    updated database model

--- database/model/tables/modules.sql
+++ database/model/tables/modules.sql
@@ -1,12 +1,11 @@
 CREATE TABLE `modules` (
   `last_update` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `name` varchar(80) NOT NULL,
-  `architecture` varchar(32) DEFAULT NULL,
-  `description` varchar(1024) DEFAULT NULL,
+  `architecture` varchar(32) NOT NULL,
   `version` varchar(32) NOT NULL,
-  `added` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `added` timestamp NOT NULL DEFAULT current_timestamp(),
   `keywords` varchar(1024) DEFAULT NULL,
   `downloads` int(11) NOT NULL DEFAULT 0,
   `repository` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`name`,`version`)
+  PRIMARY KEY (`name`,`architecture`,`version`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
